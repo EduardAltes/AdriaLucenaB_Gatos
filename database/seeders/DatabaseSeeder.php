4be17fb7bca71d2bf4aeeca16f1aa55f9825cb7e
Adri <adria.lucena@insbaixcamp.cat>
2025-04-03 19:46:25 +0200

    Arreglo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,8 +19,8 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        $this->call(CatImageSeeder::class);
     }
 
-    $this->call(CatImageSeeder::class);
     
 }
